d74838c893923fa5a86094d426ffb8c793c8538a
Shashikant Sharma <shashikant.sharma.cse12@iitbhu.ac.in>
2017-03-31 01:03:09 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting
+from .models import Tables
 
 # Create your views here.
+valid_columns=["Date" , "id" , "url" , "url_2" , "link_to_solicitation" , "agency" , "fsg" , "fsg_description" , "psc_code" , "PSC_description" , "source" , "title" , "keywords" , "email" , "phone" , "contract_specialist" , "contract_officer" , "set_aside" , "naics_code" , "contact_info" , "place_of_performance" , "description" , "Solicitation_Number" , "Notice_Type" , "Point_of_Contact" , "Point_of_Contact_email"]
+list_item= [{"id1":"Hello","url":"world","id3":"test"},{"id1":"sainyam","id2":"Kapoor","agency":"test"},]
+def populate():
+    new_entry=Tables()
+    for adict in list_item:
+        for k in adict:
+            if k in valid_columns:
+                new_entry.url=adict[k]
+                new_entry.save()
+
+
 def index(request):
     # return HttpResponse('Hello from Python!')
+    populate()
     return render(request, 'index.html')
 
 
 def db(request):
 
-    greeting = Greeting()
-    greeting.save()
+    #greeting = Greeting()
+    #greeting.save()
 
-    greetings = Greeting.objects.all()
+    #greetings = Greeting.objects.all()
 
-    return render(request, 'db.html', {'greetings': greetings})
+    return render(request, 'db.html', {'greetings': "hello"})
 
