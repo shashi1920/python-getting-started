bdf163ebd123a124d9b1d695843ebb7a858b2080
Shashikant Sharma <shashikant.sharma.cse12@iitbhu.ac.in>
2017-03-31 23:33:11 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -5,20 +5,30 @@ from .models import Tables
 
 # Create your views here.
 valid_columns=["Date" , "id" , "url" , "url_2" , "link_to_solicitation" , "agency" , "fsg" , "fsg_description" , "psc_code" , "PSC_description" , "source" , "title" , "keywords" , "email" , "phone" , "contract_specialist" , "contract_officer" , "set_aside" , "naics_code" , "contact_info" , "place_of_performance" , "description" , "Solicitation_Number" , "Notice_Type" , "Point_of_Contact" , "Point_of_Contact_email"]
-list_item= [{"id1":"Hello","url":"world","id3":"test"},{"id1":"sainyam","id2":"Kapoor","agency":"test"},]
 def populate():
+    list_item = [{"naics_code": "8258", "url": "world", "id3": "test"},
+                 {"id1": "sainyam", "id2": "Kapoor", "agency": "test"}, ]
     new_entry=Tables()
     for adict in list_item:
         for k in adict:
             if k in valid_columns:
-                new_entry.url=adict[k]
+                if k!='naics_code':
+                    setattr(new_entry,k,adict[k])
+                else:
+                    setattr(new_entry,k,int(adict[k]))
+                #new_entry.url=adict[k]
                 new_entry.save()
+
 
 
 def index(request):
     # return HttpResponse('Hello from Python!')
     populate()
-    return render(request, 'index.html')
+    print_demo=Tables.objects.all()
+
+
+    return render(request, 'index.html',context={"printd":print_demo})
+
 
 
 def db(request):
